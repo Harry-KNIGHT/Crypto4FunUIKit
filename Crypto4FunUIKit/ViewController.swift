31cbd88acc49ef9897db2b850c0e9630313d1a3a
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-16 13:24:21 +0200

    Initial Commit

---
+++ Crypto4FunUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto4FunUIKit
+//
+//  Created by Elliot Knight on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
