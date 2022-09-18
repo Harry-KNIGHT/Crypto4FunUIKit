2f6ab0607763a85db5f981d69f4b68e8e932ef80
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-18 11:09:12 +0200

    List with name, image does not appear in simulator atm.

---
+++ Crypto4FunUIKit/CryptoModel.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoModel.swift
+//  Crypto4FunUIKit
+//
+//  Created by Elliot Knight on 18/09/2022.
+//
+
+import Foundation
+import UIKit
+struct CryptoModel {
+	let imageName: String?
+	let name: String
+}
--- Crypto4FunUIKit/ViewController.swift
+++ Crypto4FunUIKit/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	var cryptos: [CryptoModel] = []
+
+	@IBOutlet weak var tableView: UITableView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		let crypto0 = CryptoModel(imageName: "crypto0", name: "Bitcoin")
+		let crypto1 = CryptoModel(imageName: "crypto1", name: "Ethereum")
+		let crypto2 = CryptoModel(imageName: nil, name: "BNB")
+
+		cryptos = [crypto0, crypto1, crypto2]
 	}
 
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return cryptos.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoCell
+
+		let currentCrypto = cryptos[indexPath.row]
+		cell.cryptoNameLabel.text = currentCrypto.name
+
+		// Image doesn't appear in simulator.
+		if let imageName = currentCrypto.imageName {
+			cell.cryptoImageView.largeContentImage = UIImage(named: imageName)
+		}
+		return cell
+	}
 
 }
+
 
