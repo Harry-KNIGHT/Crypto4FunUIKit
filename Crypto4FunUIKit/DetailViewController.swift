257eb283aa10667d3628322ade9e41953f5b4ad8
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-20 10:28:57 +0200

    Setting up

---
+++ Crypto4FunUIKit/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Crypto4FunUIKit
+//
+//  Created by Elliot Knight on 20/09/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBOutlet weak var imageView: UIImageView!
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
