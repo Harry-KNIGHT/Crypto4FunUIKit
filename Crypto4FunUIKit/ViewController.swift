5a76b44b3a1f8890d8082cad619dd43f55612d4b
Harry-KNIGHT <eliott.knight@icloud.com>
2022-09-20 21:51:08 +0200

    DetailViewController id renamed in storyboard.

--- Crypto4FunUIKit/ViewController.swift
+++ Crypto4FunUIKit/ViewController.swift
@@ -52,7 +52,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 	}
 
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+		if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController {
 			vc.selectedCrypto = cryptos[indexPath.row]
 			navigationController?.pushViewController(vc, animated: true)
 		}
